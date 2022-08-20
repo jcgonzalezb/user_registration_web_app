7c81db4b6a2d45058f7883411d20d1e1819c98ae
jcgonzalezb <jcgonzalez@gmail.com>
2022-08-20 12:43:53 -0500

    route added

--- app.py
+++ app.py
@@ -2,12 +2,13 @@
 # local packages
 from config import app, db
 from routes.home_blueprint import home_blueprint
-
+from routes.new_user_blueprint import new_user_blueprint
 
 # Create the tables that are associated with the models.
 db.create_all()
 
 app.register_blueprint(home_blueprint)
+app.register_blueprint(new_user_blueprint)
 
 if __name__ == '__main__':
     # Main entry point when run in stand-alone mode.
