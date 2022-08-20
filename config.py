0fea145341543e701a58c3d6378e0e081553057f
jcgonzalezb <jcgonzalez@gmail.com>
2022-08-20 18:02:12 -0500

    Secret key added

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 db_file = 'users.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_file
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '6dee850c-8bf4-4f1f-8dc5-729beb79eb1c'
 
 # Create a database object using the SQLAlchemy class,
 # passing the application instance to connect the Flask
