3de32309827ea232c42dc8fb21f09604830c63b7
jcgonzalezb <jcgonzalez@gmail.com>
2022-08-20 12:06:51 -0500

    file updated

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # local packages
 from config import app, db
-from routes.home_blueprint import index_blueprint
+from routes.home_blueprint import home_blueprint
 
 
 # Create the tables that are associated with the models.
 db.create_all()
 
-app.register_blueprint(index_blueprint)
+app.register_blueprint(home_blueprint)
 
 if __name__ == '__main__':
     # Main entry point when run in stand-alone mode.
