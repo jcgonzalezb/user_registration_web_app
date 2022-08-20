1bff973e08e2194a37a83e4e8af361ef4c96eb79
jcgonzalezb <jcgonzalez@gmail.com>
2022-08-20 18:48:01 -0500

    user list fixed

--- routes/user_list_blueprint.py
+++ routes/user_list_blueprint.py
@@ -1,16 +1,11 @@
 # flask packages
-from flask import Blueprint, render_template, request
-import sqlite3 as sql
+from flask import Blueprint, render_template
+
+from models.user import User
 
 user_list_blueprint = Blueprint('user_list_blueprint', __name__, url_prefix='/user_list')
 
-@user_list_blueprint.route('/', strict_slashes=False)
+@user_list_blueprint.route('/',  methods=['GET'], strict_slashes=False)
 def list():
-   con = sql.connect("users.db")
-   con.row_factory = sql.Row
-   
-   cur = con.cursor()
-   cur.execute("select * from users")
-   
-   rows = cur.fetchall()
-   return render_template("user_list.html",rows = rows)
+   users = User.query.all()
+   return render_template("user_list.html", users = users)
