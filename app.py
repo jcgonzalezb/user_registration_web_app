e43579ae483b8374657ee8c503b25de62e078b42
jcgonzalezb <jcgonzalez@gmail.com>
2022-08-20 13:03:35 -0500

    routes updated

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 from config import app, db
 from routes.home_blueprint import home_blueprint
 from routes.new_user_blueprint import new_user_blueprint
+from routes.user_list_blueprint import user_list_blueprint
 
 from flask import render_template
 
@@ -15,8 +16,8 @@ app.register_blueprint(home_blueprint)
 def add_user():
    return render_template('new_user.html')
 
-
 app.register_blueprint(new_user_blueprint)
+app.register_blueprint(user_list_blueprint)
 
 if __name__ == '__main__':
     # Main entry point when run in stand-alone mode.
