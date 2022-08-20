4a3c54141de88d89c03823e4a38044125c1efda5
jcgonzalezb <jcgonzalez@gmail.com>
2022-08-20 18:46:54 -0500

    new user fixed

--- routes/new_user_blueprint.py
+++ routes/new_user_blueprint.py
@@ -1,11 +1,23 @@
 # flask packages
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash
 
+from models.user import User
 from forms import RegisterForm
+from config import db
 
 new_user_blueprint = Blueprint('new_user_blueprint', __name__, url_prefix='/new_user')
 
-@new_user_blueprint.route('/', strict_slashes=False)
+@new_user_blueprint.route('/', methods=['GET', 'POST'], strict_slashes=False)
 def new_user():
     form = RegisterForm()
+    if form.validate_on_submit():
+        new_user = User(name=form.name.data,
+                              email=form.email.data,
+                              city=form.city.data)
+        db.session.add(new_user)
+        db.session.commit()
+    if form.errors != {}: #If there are not errors from the validations
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
+
     return render_template('new_user.html', form=form)
