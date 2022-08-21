293349c58d7579179cf83d1fa108f69c75cd64b7
jcgonzalezb <jcgonzalez@gmail.com>
2022-08-21 08:01:32 -0500

    file deleted

--- forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-
-
-class RegisterForm(FlaskForm):
-    name = StringField(label='Nombre')
-    email = StringField(label='Email')
-    city = StringField(label='Ciudad')
-    submit = SubmitField(label='Crear usuario')
