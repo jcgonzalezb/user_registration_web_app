0f045f03cbb81ad6056bdf698f28c761db5798cf
jcgonzalezb <jcgonzalez@gmail.com>
2022-08-21 12:38:23 -0500

    comment fixed

--- routes/new_user_blueprint.py
+++ routes/new_user_blueprint.py
@@ -19,6 +19,6 @@ def new_user():
         return render_template('added_user.html')
     if form.errors != {}: #If there are not errors from the validations
         for err_msg in form.errors.values():
-            flash(f'There was an error with creating a user: {err_msg}', category='danger')
+            flash(f'Hay un error creando un usuario: {err_msg}', category='danger')
 
     return render_template('new_user.html', form=form)
