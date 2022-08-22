b34528c2289e6a631fb8d80cd32d01ba3fb0cd35
jcgonzalezb <jcgonzalez@gmail.com>
2022-08-21 20:43:18 -0500

    unittest done

--- tests/test_user.py
+++ tests/test_user.py
@@ -3,21 +3,32 @@
 """
 import unittest
 
-
 # project resource
+from config import app, db
 from models.user import User
+
+db_file = 'users.db'
 
 
 class TestUser(unittest.TestCase):
-    """This class contains several methods to test the
-    user.py file.
-    """
+    """Test the User class"""
+
     def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_file
+        self.app = app.test_client()
+        db.drop_all()
+        db.create_all()
+
+    # executed after each test
+
+    def tearDown(self):
         pass
 
-    def test(self):        
+    def test(self):
         self.assertTrue(True)
-
 
 
 if __name__ == '__main__':
